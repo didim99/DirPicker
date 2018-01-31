e673c029183551f0c2a91579de292fb0f6d6bec3
didim99 <didim@eclabs.ru>
2018-01-31 23:41:18 +0300

    Fixed some crashes

--- java/DirPickerActivity.java
+++ java/DirPickerActivity.java
@@ -5,15 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,14 +32,16 @@ import java.util.Map;
 
 public class DirPickerActivity extends AppCompatActivity {
   private static final String LOG_TAG = "My_log_DirPicker";
+  private Context appContext;
   private TextView textPath;
+  private Button btnGo;
   private int selectedId = -1;
   private String path;
 
+  private final String FS_ROOT = "/";
   private final String ATTR_NAME = "name";
   private final String ATTR_IS_DIR = "isDir";
   private final String KEY_DIR_PICKER_LAST_PATH = "dirPicker_lastPath";
-  private final String DEFVALUE_DIR_PICKER_LAST_PATH = "/";
 
   private ArrayList<Map<String, Object>> arrayDir = new ArrayList<>();
   private ListDirAdapter adapter;
@@ -44,10 +50,12 @@ public class DirPickerActivity extends AppCompatActivity {
     Log.d(LOG_TAG, "DirPickerActivity starting...");
     super.onCreate(savedInstanceState);
     setContentView(R.layout.dir_picker);
+    appContext = getApplicationContext();
 
     //Adapter and view configuration
     adapter = new ListDirAdapter(this, arrayDir);
     textPath = findViewById(R.id.textPath);
+    btnGo = findViewById(R.id.dirPicker_go);
     RecyclerView listDir = findViewById(R.id.list_dir);
     listDir.setLayoutManager(new LinearLayoutManager(this));
     listDir.setHasFixedSize(true);
@@ -56,8 +64,17 @@ public class DirPickerActivity extends AppCompatActivity {
     //loading path from saved instance or from SharedPreferences
     path = (String) getLastCustomNonConfigurationInstance();
     if (path == null)
-      path = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
-        .getString(KEY_DIR_PICKER_LAST_PATH, DEFVALUE_DIR_PICKER_LAST_PATH);
+      path = PreferenceManager.getDefaultSharedPreferences(appContext)
+        .getString(KEY_DIR_PICKER_LAST_PATH, FS_ROOT);
+    // Checking access to file system root directory
+    if (path.equals(FS_ROOT) && !isDirOpened(path)) {
+      Toast.makeText(appContext,
+        R.string.dirPicker_fsRootUnreadable, Toast.LENGTH_LONG).show();
+      path = Environment.getExternalStorageDirectory().getPath() + "/";
+      //save new start path in SharedPreferences
+      PreferenceManager.getDefaultSharedPreferences(appContext).edit()
+        .putString(KEY_DIR_PICKER_LAST_PATH, path).apply();
+    }
     updateListDir();
 
     Log.d(LOG_TAG, "DirPickerActivity started");
@@ -69,12 +86,12 @@ public class DirPickerActivity extends AppCompatActivity {
   }
 
   public void onClickBack (View view) {
-    if (path.equals("/")) {
+    if (path.equals(FS_ROOT)) {
       setResult(Activity.RESULT_CANCELED, new Intent());
       finish();
     } else {
       path = (new File(path)).getParent();
-      if (!path.equals("/"))
+      if (!path.equals(FS_ROOT))
         path += "/";
       updateListDir();
     }
@@ -82,7 +99,7 @@ public class DirPickerActivity extends AppCompatActivity {
 
   public void onClickGo (View view) {
     //save last picked path in SharedPreferences
-    PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit()
+    PreferenceManager.getDefaultSharedPreferences(appContext).edit()
       .putString(KEY_DIR_PICKER_LAST_PATH, path).apply();
     Intent intent = new Intent();
     intent.setData(Uri.parse("file://" + path));
@@ -99,26 +116,33 @@ public class DirPickerActivity extends AppCompatActivity {
 
     ArrayList<Map<String, Object>> arrayFiles = new ArrayList<>();
     File[] files = new File(path).listFiles();
-    Arrays.sort(files);
 
-    Map<String, Object> m;
-    for (File file : files) {
-      boolean isDir = file.isDirectory();
-      m = new HashMap<>();
-      m.put(ATTR_NAME, file.getName());
-      m.put(ATTR_IS_DIR, isDir);
-      if (isDir && dirOpened(file.getAbsolutePath()))
-        arrayDir.add(m);
-      else if (!isDir)
-        arrayFiles.add(m);
+    boolean dirOpened = files != null;
+    btnGo.setEnabled(dirOpened);
+    if (dirOpened) {
+      Arrays.sort(files);
+      Map<String, Object> m;
+      for (File file : files) {
+        boolean isDir = file.isDirectory();
+        m = new HashMap<>();
+        m.put(ATTR_NAME, file.getName());
+        m.put(ATTR_IS_DIR, isDir);
+        if (isDir)
+          arrayDir.add(m);
+        else
+          arrayFiles.add(m);
+      }
+      arrayDir.addAll(arrayFiles);
+    } else {
+      Toast.makeText(appContext,
+        R.string.dirPicker_dirUnreadable, Toast.LENGTH_LONG).show();
     }
-    arrayDir.addAll(arrayFiles);
 
     adapter.notifyDataSetChanged();
     textPath.setText(path);
   }
 
-  private boolean dirOpened(String dirName) {
+  private boolean isDirOpened(String dirName) {
     try {
       File[] files = new File(dirName).listFiles();
       for (File file : files) {}
